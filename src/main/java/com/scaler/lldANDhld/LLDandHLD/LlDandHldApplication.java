37373f111b876b660452a58e10bff4b87e5f0494
Kamlesh Tripathi <kamleshtripathi44@gmail.com>
2023-03-25 10:32:32 +0530

    Initial commit

---
+++ src/main/java/com/scaler/lldANDhld/LLDandHLD/LlDandHldApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.lldANDhld.LLDandHLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlDandHldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlDandHldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/lldANDhld/LLDandHLD/LlDandHldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.lldANDhld.LLDandHLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlDandHldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
